d0b3a207f4d3db42ff2eb61b8b7f436815994212
yangjianyun <yangjianyun@58.com>
2021-11-16 15:19:18 +0800

    webpack5配置

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -13,7 +13,7 @@ module.exports = {
     entry: path.resolve(__dirname, "../src/index.js"),
     output: {
         path: path.resolve(__dirname, '../dist'),
-        filename: 'index_bundle.js',
+        filename: '[name].bundle.js',
     },
     plugins: [new HtmlWebpackPlugin({
         title: "react-antd-template-admin",
