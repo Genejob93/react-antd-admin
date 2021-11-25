6118e2aff6536b17bcc411d5c6056d73143eb32d
yangjianyun <yangjianyun@58.com>
2021-11-25 12:08:45 +0800

    修改配置

--- webpack.config.js
+++ webpack.config.js
@@ -6,20 +6,18 @@
  * @description: webpack的配置文件,根据不同需求,加载不同环境配置
  */
 const path = require("path")
-const {
-  merge
-} = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const baseConfig = require(path.resolve(__dirname, "config/webpack.base.config"))
 const devConfig = require(path.resolve(__dirname, "config/webpack.dev.config"))
 const prodConfig = require(path.resolve(__dirname, "config/webpack.prod.config"))
 
 module.exports = () => {
-  switch (process.env.NODE_ENV) {
-    case 'development':
-      return merge(baseConfig, devConfig);
-    case 'production':
-      return merge(baseConfig, prodConfig);
-    default:
-      throw new Error('No matching configuration was found!');
-  }
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            return merge(baseConfig, devConfig);
+        case 'production':
+            return merge(baseConfig, prodConfig);
+        default:
+            throw new Error('No matching configuration was found!');
+    }
 }
