3f44af0e0e58f69098ee7ccc85c22166dad045e0
yangjianyun <yangjianyun@58.com>
2021-11-25 12:07:15 +0800

    添加 项目没目录

---
+++ src/router/index.js
@@ -0,0 +1,7 @@
+/**
+ * @author: Gene
+ * @age: 永远18岁的美少年
+ * @Email： yangjianyun@58.com
+ * @date: 2021-11-25 12:06:00
+ * @description: router 路由文件
+ */
---
+++ src/utils/index.js
@@ -0,0 +1,7 @@
+/**
+* @author: Gene
+* @age: 永远18岁的美少年
+* @Email： yangjianyun@58.com
+* @date: 2021-11-25 12:04:03
+* @description: utils 工具类
+*/
